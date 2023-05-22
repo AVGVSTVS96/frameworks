b96437f064ca5ad62cadc2f9781191fc4ca89b58
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-05-21 21:03:52 -0400

    add comments

--- react/myapp/src/App.js
+++ react/myapp/src/App.js
@@ -8,12 +8,21 @@ import './App.css';  // import css file
 function App() {
 
   // useState hook 
+  //  - useState is a function that takes the initial state as an argument
+  //  - The initial state is an empty array so todos will start as an empty list
+    
+    // useState returns an array with two elements
+    //  - todos is the current state of the element
+    //  - setTodos is a function that can be used to update the state of todos  
   const [todos, setTodos] = useState([]);
 
-  // Using useRef hook to get values from DOM elements
+  // useRef hook to get/reference values from DOM elements
+  //  - returns the value of the input field
   const todoText = useRef();
 
   // Side Effects / Lifecycle
+  // useEffect is a hook to run side effects like fetching data and updating the DOM
+  // useEffect hook is used to read todos from local storage
   useEffect(() => {
     const existingTodos = localStorage.getItem('todos');
     setTodos(existingTodos ? JSON.parse(existingTodos) : []);
