0eec41fa5ecddb326f30b63c6406dbaac105aa2e
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-05-22 01:10:19 -0400

    naming change

--- svelte/svelte-project/src/App.svelte
+++ svelte/svelte-project/src/App.svelte
@@ -23,15 +23,15 @@
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-  <div class="card">
+      <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        <img src={viteLogo} class="logo" alt="Vite Logo" />
+      </a>
+      <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+        <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      </a>
+    </div>
+    <h1>Vite + Svelte</h1>
+    <div class="card">
     <Counter />
   </div>
 
