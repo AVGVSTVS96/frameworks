b7a2d64286fe575dd84cfe84d1f9b6d415cc0128
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-05-22 00:53:23 -0400

    functional solid.js todo app

--- solidjs/my-app/src/App.jsx
+++ solidjs/my-app/src/App.jsx
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { createSignal, onMount } from 'solid-js';
 
 function App() {
+  
+  const [todos, setTodos] = createSignal([]);
+  let todoText;
+
+  onMount(() => {
+    const existingTodos = localStorage.getItem('todos');
+    setTodos(existingTodos ? JSON.parse(existingTodos) : []);
+  });
+
+  function addTodo(event) {
+    event.preventDefault();
+    const next = [...todos(), todoText.value];
+    setTodos(next);
+    localStorage.setItem('todos', JSON.stringify(next));
+    todoText.value = '';
+  }
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        <div>
+      <ul>
+        {todos().map(todo => (<li key={todo}>{todo}</li>))}  
+      </ul>
+
+      <form onSubmit={addTodo}>
+        <input type="text" placeholder="What needs to be done?" ref={todoText} />
+        <input type="submit" value="Add Todo" />
+      </form>
+    </div>
       </header>
     </div>
   );
